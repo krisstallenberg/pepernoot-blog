34266ae1f79eed6c242e0eebfa1c19f7bffb3be8
krisstallenberg <krisstallenberg@gmail.com>
2023-09-03 16:50:12 +0200

    Install tailwind CSS

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,9 @@ import Layout from "../components/layout"
 const IndexPage = () => {
   return (
     <Layout>
-      <h1>Home</h1>
-      <p>test</p>
+      <h1 className="font-sans font-bold my-8">Home</h1>
+      <p className="font-sans font-light text-slate-500 my-5">test</p>
     </Layout>
-
   )
 }
 
